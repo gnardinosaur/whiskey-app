1ed6bffbce3c14ab378e9e9e7a30193a5e8d4086
gnardinosaur <github email address>
2020-03-31 17:25:59 -0400

    top 10 card transitions and link-outs working

--- components/TopTen.js
+++ components/TopTen.js
@@ -3,14 +3,21 @@ import { withGoogleSheets } from 'react-db-google-sheets';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 
-const useStyles = makeStyles(theme => ({
-  media: {
-    height: 500
+const useStyles = makeStyles({
+  card: {
+    height: 500,
+    opacity: 1,
+    display: 'block',
+    width: '100%',
+    transition: '.5s ease',
   }
-}));
+});
+
+function linkOut(e) {
+  window.open(e.target.closest('#top-ten-card').dataset.link, '_blank')
+}
 
 function TopTen(props) {
   const classes = useStyles();
@@ -24,12 +31,14 @@ function TopTen(props) {
     <Grid container justify='center' spacing={4}>
       {topTen.map(el =>
         <Grid item key={el.Whiskey} xs={6}>
-          <Card>
-            <CardHeader title={el.Whiskey} />
-            <CardMedia 
-              className={classes.media}
-              image={el.Image}
-            />  
+          <Card id='top-ten-card' data-link={el.Link} onClick={linkOut}>
+            <CardMedia id='top-ten-card-img' className={classes.card} image={el.Image} />
+            <div className='top-ten-card-middle'>
+              <div className='top-ten-card-text'>#{el.Rank}</div>
+              <br />
+              <div className='top-ten-card-text'>{el.Whiskey}</div>
+              <div className='top-ten-card-text-2'>{el.Theme}</div>
+            </div> 
           </Card>
         </Grid>)}
     </Grid>
