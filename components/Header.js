adeba541db89b88d97e183e45ad91feb7483d72a
gnardinosaur <github email address>
2020-03-31 20:42:08 -0400

    links/buttons/router working

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import Button from '@material-ui/core/Button';
 
 function Header() {
@@ -6,9 +8,15 @@ function Header() {
     <div className='header'>
       <h1>Whiskey Club NYC</h1>
       <div>
-        <Button variant="outlined" id='header-btn'>Home</Button>
-        <Button variant="outlined" id='header-btn'>Our Whiskies</Button>
-        <Button variant="outlined" id='header-btn'>Rate</Button>
+        <Link to='/' className='router-link'>
+          <Button variant="outlined" id='header-btn'>Home</Button>
+        </Link>
+        <Link to='/whiskies' className='router-link'>
+          <Button variant="outlined" id='header-btn'>Our Whiskies</Button>
+        </Link>
+        <Link to='/rate' className='router-link'>
+          <Button variant="outlined" id='header-btn'>Rate</Button>
+        </Link>
       </div>
     </div>
   )
