ae9851d0029d8dd43104a7fdec46415a7e49153c
gnardinosaur <github email address>
2020-04-10 17:33:30 -0400

    posting to google sheets working

--- helpers/googleAPI.js
+++ helpers/googleAPI.js
@@ -1,50 +1,54 @@
-  // secrets from .env
-  const SPREADSHEET_ID = process.env.REACT_APP_GOOGLE_SHEETS_DOC_ID;
-  const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
-  const API_KEY = process.env.REACT_APP_GOOGLE_SHEETS_API_KEY;
   const SCOPE = 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/spreadsheets';
-  const range = 'test';
 
   // initialize Google API
-  export function initClient() {
-    gapi.load('client:auth2', authenticate)
+  export function initClient(cb) {
+    gapi.load('client:auth2', () => authenticate(cb))
   };
 
   // provide 0Auth credentials
-  function authenticate() {
+  function authenticate(cb) {
     gapi.client.init({
-      'client_id': CLIENT_ID,
-      'apiKey': API_KEY,
+      'client_id': process.env.REACT_APP_GOOGLE_CLIENT_ID,
+      'apiKey': process.env.REACT_APP_GOOGLE_SHEETS_API_KEY,
       'scope': SCOPE,
       'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest?version=v4']
     })
-    .then(googleSignIn)
+    .then(() => googleSignIn(cb))
   };
 
   // sign-in using 0Auth with scopes so we can read/write sheet with GoogleSheets API -- log success or errors to console for debugging
-  function googleSignIn() {
+  function googleSignIn(cb) {
     return gapi.auth2.getAuthInstance()
       .signIn({scope: SCOPE})
-      .then(function() { console.log("Sign-in successful"); },
-            function(err) { console.error("Error signing in", err); })
-  }
-
-  export function execute() {
-    console.log('execute')
-    // return gapi.client.sheets.spreadsheets.values.append({
-    //   "spreadsheetId": SPREADSHEET_ID,
-    //   "key": API_KEY,
-    //   "range": range,
-    //   "valueInputOption": "RAW",
-    //   "resource": {
-    //     "values": [
-    //       [input]
-    //     ]
-    //   }
-    // })
-    // .then(function(response) {
-    //   // Handle the results here (response.result has the parsed body).
-    //   console.log("Response", response);
-    //   },
-    //   function(err) { console.error("Execute error", err); });
+      .then(function() { console.log("Sign-in successful"); }, function(err) { console.error("Error signing in", err); })
+      .then(() => cb())
   };
+
+  export function cleanFormData(data, dataArr = []) {
+    let values = Object.values(data);
+    values.map(el => {
+      if(el instanceof Array) {
+        cleanFormData(el, dataArr)
+      } else {
+        dataArr.push(el)
+      }
+    })
+    return dataArr
+  };
+
+  export function postFormData(data) {
+    return gapi.client.sheets.spreadsheets.values.append({
+      "spreadsheetId": process.env.REACT_APP_GOOGLE_SHEETS_DOC_ID,
+      "key": process.env.REACT_APP_GOOGLE_SHEETS_API_KEY,
+      "range": 'test',
+      "valueInputOption": "RAW",
+      "resource": {
+        "values": [data]
+      }
+    })
+    .then(function(response) {
+      // Handle the results here (response.result has the parsed body).
+      console.log("Response", response);
+      },
+      function(err) { console.error("Execute error", err); });
+  }
