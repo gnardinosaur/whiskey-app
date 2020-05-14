84b9cc0da86d862007b5718563b77bb2200ff0e5
gnardinosaur <github email address>
2020-05-14 16:08:02 -0400

    RateForm and RateInput styles moved to .scss and/or within component

--- components/Rate/index.js
+++ components/Rate/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import BannerWrap from '../BannerWrap';
-import RateForm from '../RateForm';
+import RateForm from '../RateForm/index';
 import styles from './styles.scss';
 
 function Rate() {
