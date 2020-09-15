e35c74c9feb212271e1af1360c76172a6de3f86d
gnardinosaur <github email address>
2020-09-15 08:44:36 -0400

    removed \".\" from homepage copy

--- components/Home/index.js
+++ components/Home/index.js
@@ -13,7 +13,7 @@ function Home(props) {
         <div className={styles.homeTop}>
           <div>
             <div className={styles.homeTopContent}>
-              <h1>Welcome to Whiskey Club NYC.</h1>
+              <h1>Welcome to Whiskey Club NYC</h1>
               <h3>"Too much of anything is bad, but too much good whiskey is barely enough."</h3>
               <h3 className={styles.authorQuote}>- Mark Twain</h3>
               <Link to='/our-whiskies' className='router-link'>
